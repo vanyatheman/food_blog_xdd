aa53e3799c5b1d4bc290c97fee8ee4f23c104d35
Ivan Mikheev <ivan.a.mikheev@yandex.ru>
2024-05-17 17:09:34 +0700

    docker compose dev update.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -109,7 +109,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # STATICFILES_DIRS = ((BASE_DIR / 'static/'),)
-STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
